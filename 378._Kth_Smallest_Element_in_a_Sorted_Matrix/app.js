d5decec76d942b54c649b1ce9d8fc78903f7273e
Agah Özdemir <aa.ozdemir2018@gtu.edu.tr>
2022-08-02 17:27:51 +0300

    Add files via upload

--- 378._Kth_Smallest_Element_in_a_Sorted_Matrix/app.js
+++ 378._Kth_Smallest_Element_in_a_Sorted_Matrix/app.js
@@ -1,26 +1,28 @@
+// 378. Kth Smallest Element in a Sorted Matrix
+// Given an n x n matrix where each of the rows and columns is sorted in ascending order, return the kth smallest element in the matrix.
+// Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+// You must find a solution with a memory complexity better than O(n2).
+
 var kthSmallest = function (matrix, k) {
-    const newArray = [];
-    for (let i = 0; i < matrix.length; i++) {
-      for (let j = 0; j < matrix[i].length; j++) {
-        if (matrix.length != matrix[i].length || 1 >= matrix.length >= 300) {
-          return false;
-        }
-        newArray.push(matrix[i][j]);
+  const newArray = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix.length != matrix[i].length || 1 >= matrix.length >= 300) {
+        return false;
+      }
+      newArray.push(matrix[i][j]);
+    }
+  }
+  let temp = newArray[0];
+
+  for (let i = 0; i < newArray.length; i++) {
+    for (let j = 0; j < newArray.length - i - 1; j++) {
+      if (newArray[j] > newArray[j + 1]) {
+        temp = newArray[j];
+        newArray[j] = newArray[j + 1];
+        newArray[j + 1] = temp;
       }
     }
-    let temp = newArray[0];
-  
-    for (let i = 0; i < newArray.length; i++)
-    {
-        for (let j = 0; j < newArray.length - i - 1; j++)
-        {
-            if (newArray[j] > newArray[j + 1])
-            {
-                temp = newArray[j];
-                newArray[j] = newArray[j + 1];
-                newArray[j+1] = temp;
-            }
-        }
-    }
-    return newArray[k-1];
-  };
+  }
+  return newArray[k - 1];
+};
